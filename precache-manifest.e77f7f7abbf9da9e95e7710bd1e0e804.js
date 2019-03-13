d07db4308021af2bbf9943fe2d0acd57634bdcf2
Emil Bisak <bisak.emil@gmail.com>
2019-03-13 18:27:57 +0100

    Updates

---
+++ precache-manifest.e77f7f7abbf9da9e95e7710bd1e0e804.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "839aaf4cf1e0807c8af281ee6da22f7c",
+    "url": "/slot_game/static/media/banana.839aaf4c.png"
+  },
+  {
+    "revision": "6cb693a5c357f5ff34dd",
+    "url": "/slot_game/static/css/main.b2bfd8f6.chunk.css"
+  },
+  {
+    "revision": "6eb0367f924a3bad79e9",
+    "url": "/slot_game/static/js/runtime~main.6eb0367f.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/slot_game/static/js/2.ac164eeb.chunk.js"
+  },
+  {
+    "revision": "5f23eddbe0e264f28b73f13e431467c1",
+    "url": "/slot_game/static/media/47.5f23eddb.jpg"
+  },
+  {
+    "revision": "f261b10542f6c205acf726e49b17f47e",
+    "url": "/slot_game/static/media/slots-design.f261b105.jpg"
+  },
+  {
+    "revision": "5419a33c5eeffab8be06e5635449b00c",
+    "url": "/slot_game/static/media/winning_slot.5419a33c.wav"
+  },
+  {
+    "revision": "e67f7809bb275d69bb5637740ebcdca5",
+    "url": "/slot_game/static/media/olive.e67f7809.png"
+  },
+  {
+    "revision": "762b5d84ea4aa557048856f8c3680a52",
+    "url": "/slot_game/static/media/apricot.762b5d84.png"
+  },
+  {
+    "revision": "0b355c2fee940132fe5f19d167aed15f",
+    "url": "/slot_game/static/media/strawberry.0b355c2f.png"
+  },
+  {
+    "revision": "a1e75a2604c7245055e20a2a4f174b3d",
+    "url": "/slot_game/static/media/grape.a1e75a26.png"
+  },
+  {
+    "revision": "ee6d1686c036352b6e4af21e06d14f72",
+    "url": "/slot_game/static/media/apple.ee6d1686.png"
+  },
+  {
+    "revision": "fd7f71dd6bd11b3a7e1a69c134c43f64",
+    "url": "/slot_game/static/media/orange.fd7f71dd.png"
+  },
+  {
+    "revision": "91e9cd99289014433f2644d12e5cfdb7",
+    "url": "/slot_game/static/media/cherry.91e9cd99.png"
+  },
+  {
+    "revision": "5764f3e3cc03da77e71b9c6881295962",
+    "url": "/slot_game/static/media/lime.5764f3e3.png"
+  },
+  {
+    "revision": "6cb693a5c357f5ff34dd",
+    "url": "/slot_game/static/js/main.6cb693a5.chunk.js"
+  },
+  {
+    "revision": "ccf162febb68541699a20d60a4b7ee2e",
+    "url": "/slot_game/static/media/watermelon.ccf162fe.png"
+  },
+  {
+    "revision": "1ec6c79dec56389ed7fe66db6e11b89e",
+    "url": "/slot_game/static/media/clover.1ec6c79d.png"
+  },
+  {
+    "revision": "e9d9e954fe6739cb077119f09f6703e9",
+    "url": "/slot_game/static/media/diamond.e9d9e954.png"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/slot_game/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "bde543d15235944bc3c1e0619a48ebcb",
+    "url": "/slot_game/static/media/olives.bde543d1.jpg"
+  },
+  {
+    "revision": "6b73acb5a562aa460010a15911a6bdf2",
+    "url": "/slot_game/static/media/apricots.6b73acb5.jpg"
+  },
+  {
+    "revision": "6d4c41f841a432f0d873ac26b7690c44",
+    "url": "/slot_game/static/media/strawberries.6d4c41f8.jpg"
+  },
+  {
+    "revision": "7ed64432def8ad5fd9a09d29bf86d946",
+    "url": "/slot_game/static/media/grapes.7ed64432.jpg"
+  },
+  {
+    "revision": "dab97efc50b89477e77760cac19f8c00",
+    "url": "/slot_game/static/media/cheries.dab97efc.jpg"
+  },
+  {
+    "revision": "621c5de850902b1487f37ca9d4512f31",
+    "url": "/slot_game/static/media/limes.621c5de8.jpg"
+  },
+  {
+    "revision": "5733636210ab1d88ac488e4aeb77b871",
+    "url": "/slot_game/static/media/bananas.57336362.jpg"
+  },
+  {
+    "revision": "7933a09654c120916a3c18137a442ee0",
+    "url": "/slot_game/static/media/watermellons.7933a096.jpg"
+  },
+  {
+    "revision": "b8df65898b58cc3c6aef9c7eefcda508",
+    "url": "/slot_game/static/media/clovers.b8df6589.jpg"
+  },
+  {
+    "revision": "0f8e2c92f795664023dfdb874a976941",
+    "url": "/slot_game/static/media/diamonds.0f8e2c92.jpg"
+  },
+  {
+    "revision": "3d26eea72f072dde24cb33c1ee2f05cf",
+    "url": "/slot_game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/slot_game/precache-manifest.080e6b09682d7f41be48d415983e2a2c.js"
+  "/slot_game/precache-manifest.e77f7f7abbf9da9e95e7710bd1e0e804.js"
 );
 
 workbox.clientsClaim();
